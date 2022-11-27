2fc18246ad65bbc3b8ee4ec234cec34b3b849e47
Jeremy Evans <jeremy@chiefgeek.io>
2022-11-27 06:52:44 -0500

    Added nav to site

--- portfolio/nuxt.config.ts
+++ portfolio/nuxt.config.ts
@@ -1,4 +1,7 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
 
-})
+export default defineNuxtConfig({
+  css: ["~/assets/css/tailwind.css"],
+  modules: [
+    '@nuxtjs/tailwindcss',
+  ]
+});
---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  purge: [
+    "./components/**/*.{vue,js}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'sans':['Helvetica']
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
