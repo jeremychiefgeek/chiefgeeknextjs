fe5dbac734dc7da073a3153f07c59db2b55002aa
Jeremy Evans <jeremy@chiefgeek.io>
2022-12-05 10:23:50 -0500

    About Us Section is done

--- portfolio/pages/index.vue
+++ portfolio/pages/index.vue
@@ -10,21 +10,38 @@
         </div>
     </div>
     <div class=" bg-orange-500/5">
-        <div class="container m-auto grid grid-cols-12 pt-20 pb-20">
+        <div class="container m-auto grid grid-cols-12 pt-32 pb-32 gap-28">
             <div class="col-span-6">
                 <div class="grid grid-cols-4 gap-4">
                     <div class="bg-blue-900/75 w-auto h-auto col-span-1"></div>
-                    <img src="/images/coders.jpg" class=" object-cover w-auto col-span-3" />
-                    <img src="/images/parent_child_learning.jpg" class=" object-cover w-auto col-span-3" />
-                    <div class="bg-orange-500/75 w-auto h-auto col-span-1"></div>
+                    <img src="/images/coders.jpg" class=" object-cover w-full h-[200px] col-span-3" />
+                    <img src="/images/parent_child_learning.jpg" class=" object-cover w-full h-[200px] col-span-3" />
+                    <div class="bg-orange-500/75 w-auto h-auto col-span-d1 place-items-start"></div>
                 </div>
             </div>
-            <!--<div class="cols-span-6">
-                <h1>Testing this</h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
-                    Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            </div>-->
+            <div class="col-span-6 font-sans">
+                <div class="grid grid-cols-4 gap-4">
+                    <p class="text-orange-500 font-sans col-span-4 text-2xl p-2">About Us</p>
+                    <p class="col-span-4 w-full p-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                        when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
+                        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <div class="grid grid-cols-6 gap-8 col-span-4 text-center place-items-center pt-7">
+                        <div class="col-span-2 w-full">
+                            <p class="font-bold text-4xl">07 +</p>
+                            <p class="font-regular text-xs pt-4 uppercase">Years of operation</p>
+                        </div>
+                        <div class="col-span-2">
+                            <p class="font-bold text-4xl">70k</p>
+                            <p class="font-regular text-xs pt-4 uppercase">Lines of code written</p>
+                        </div>
+                        <div class="col-span-2">
+                            <p class="font-bold text-4xl">Countless</p>
+                            <p class="font-regular text-xs pt-5 uppercase">People Helped</p>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
         </div>
     </div>
 </template>
